fb135bfa51b626fdafbc81f935bd5951ef5dcf66
justincave <58671356+justincave@users.noreply.github.com>
2023-11-29 08:06:02 -0500

    Update BQ_Analyses.sql

--- BQ_Analyses.sql
+++ BQ_Analyses.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE TABLE postgis.MA_Results AS (
+CREATE OR REPLACE TABLE postgis.State_Results AS (
 
-SELECT * FROM `postgis.MA_State_BSL` as A
+SELECT * FROM `postgis.State_BSL` as A
 LEFT JOIN (SELECT DISTINCT BSL_ID as bsl_id25x3 FROM `postgis.all_25x3CN`) as B ON B.bsl_id25x3=A.BSL_ID --served 25x3
 LEFT JOIN (SELECT DISTINCT BSL_ID as bsl_idwire25x3 FROM `postgis.all_25x3CN`  WHERE tech IN ('Fiber','DSL','Cable')) as C ON C.bsl_idwire25x3=A.BSL_ID --served wireline 25x3
 LEFT JOIN (SELECT DISTINCT BSL_ID as bsl_idwireless25x3 FROM `postgis.all_25x3CN` WHERE tech ='Fixed Wireless') as D ON D.bsl_idwireless25x3=A.BSL_ID --served wireless 25x3
@@ -59,9 +59,9 @@ GROUP BY bsl_id, provider)) S ON S.uniq1Gx1GBSL=A.BSL_ID
 );
 
 
---ADD ALL THE FORMATING FIELDS THAT MA REQUESTED
+--ADD ALL THE FORMATING FIELDS THAT State REQUESTED
 
-ALTER TABLE postgis.MA_Results
+ALTER TABLE postgis.State_Results
 ADD COLUMN IF NOT EXISTS Served25x3 STRING,
 ADD COLUMN IF NOT EXISTS ServedWireline25x3 STRING,
 ADD COLUMN IF NOT EXISTS ServedWireless25x3 STRING,
@@ -89,31 +89,31 @@ ADD COLUMN IF NOT EXISTS NumProviders100x20 INT64,
 ADD COLUMN IF NOT EXISTS NumProviders100x100 INT64,
 ADD COLUMN IF NOT EXISTS NumProviders1Gx1G INT64
 ;
-UPDATE `postgis.MA_Results` SET Served25x3 = 'Yes' WHERE bsl_id25x3 IS NOT NULL; 
-UPDATE `postgis.MA_Results` SET ServedWireline25x3 = 'Yes' WHERE bsl_idwire25x3 IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET ServedWireless25x3 = 'Yes' WHERE bsl_idwireless25x3 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET Served25x3 = 'Yes' WHERE bsl_id25x3 IS NOT NULL; 
+UPDATE `postgis.State_Results` SET ServedWireline25x3 = 'Yes' WHERE bsl_idwire25x3 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET ServedWireless25x3 = 'Yes' WHERE bsl_idwireless25x3 IS NOT NULL ;
 
-UPDATE `postgis.MA_Results` SET Served100x20 = 'Yes' WHERE bsl_id100x20 IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET ServedWireline100x20 = 'Yes' WHERE bsl_idwire100x20 IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET ServedWireless100x20 = 'Yes' WHERE bsl_idwireless100x20 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET Served100x20 = 'Yes' WHERE bsl_id100x20 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET ServedWireline100x20 = 'Yes' WHERE bsl_idwire100x20 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET ServedWireless100x20 = 'Yes' WHERE bsl_idwireless100x20 IS NOT NULL ;
 
-UPDATE `postgis.MA_Results` SET Served100x100 = 'Yes' WHERE bsl_id100x100 IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET ServedWireline100x100 = 'Yes' WHERE bsl_idwire100x100 IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET ServedWireless100x100 = 'Yes' WHERE bsl_idwireless100x100 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET Served100x100 = 'Yes' WHERE bsl_id100x100 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET ServedWireline100x100 = 'Yes' WHERE bsl_idwire100x100 IS NOT NULL ;
+UPDATE `postgis.State_Results` SET ServedWireless100x100 = 'Yes' WHERE bsl_idwireless100x100 IS NOT NULL ;
 
-UPDATE `postgis.MA_Results` SET Served1Gx1G = 'Yes' WHERE bsl_id1Gx1G IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET ServedWireline1Gx1G = 'Yes' WHERE bsl_idwire1Gx1G IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET ServedWireless1Gx1G = 'Yes' WHERE bsl_idwireless1Gx1G IS NOT NULL ;
+UPDATE `postgis.Staet_Results` SET Served1Gx1G = 'Yes' WHERE bsl_id1Gx1G IS NOT NULL ;
+UPDATE `postgis.State_Results` SET ServedWireline1Gx1G = 'Yes' WHERE bsl_idwire1Gx1G IS NOT NULL ;
+UPDATE `postgis.State_Results` SET ServedWireless1Gx1G = 'Yes' WHERE bsl_idwireless1Gx1G IS NOT NULL ;
 
 --max down/up wireline
-UPDATE `postgis.MA_Results` SET MaxDownWireline = maxwiredown  WHERE maxwireBSL IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET MaxUpWireline = maxwireup  WHERE maxwireBSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET MaxDownWireline = maxwiredown  WHERE maxwireBSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET MaxUpWireline = maxwireup  WHERE maxwireBSL IS NOT NULL ;
 --max down/up wireless
-UPDATE `postgis.MA_Results` SET MaxDownWireless = maxwirelessdown  WHERE maxwirelessBSL IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET MaxUpWireless = maxwirelessup  WHERE maxwirelessBSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET MaxDownWireless = maxwirelessdown  WHERE maxwirelessBSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET MaxUpWireless = maxwirelessup  WHERE maxwirelessBSL IS NOT NULL ;
 
 --unique number of providers
-UPDATE `postgis.MA_Results` SET NumProviders25x3 = uniq25x3  WHERE uniq25x3BSL IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET NumProviders100x20 = uniq100x20  WHERE uniq100x20BSL IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET NumProviders100x100 = uniq100x100  WHERE uniq100x100BSL IS NOT NULL ;
-UPDATE `postgis.MA_Results` SET NumProviders1Gx1G = uniq1000x1000  WHERE uniq1Gx1GBSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET NumProviders25x3 = uniq25x3  WHERE uniq25x3BSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET NumProviders100x20 = uniq100x20  WHERE uniq100x20BSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET NumProviders100x100 = uniq100x100  WHERE uniq100x100BSL IS NOT NULL ;
+UPDATE `postgis.State_Results` SET NumProviders1Gx1G = uniq1000x1000  WHERE uniq1Gx1GBSL IS NOT NULL ;
